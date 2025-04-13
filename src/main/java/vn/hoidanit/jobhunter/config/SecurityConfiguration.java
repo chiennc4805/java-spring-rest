ac4c9311d2510fa580abf716d9512e276c9c725c
chiennc4805 <chiennguyen5981@gmail.com>
2025-04-13 09:59:46 +0700

    update

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration {
             CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         String[] whiteList = {
                 "/", "/api/v1/auth/**", "/storage/**", "/api/v1/skills/**",
-                "/api/v1/companies/**", "/api/v1/jobs/**", "/api/v1/files"
+                "/api/v1/companies/**", "/api/v1/jobs/**", "/api/v1/files", "/api/v1/email/**"
         };
         http
                 .csrf(c -> c.disable())
--- src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/SubscriberService.java
@@ -6,8 +6,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.jobhunter.domain.Job;
 import vn.hoidanit.jobhunter.domain.Skill;
 import vn.hoidanit.jobhunter.domain.Subscriber;
+import vn.hoidanit.jobhunter.domain.response.email.ResEmailJob;
+import vn.hoidanit.jobhunter.repository.JobRepository;
 import vn.hoidanit.jobhunter.repository.SkillRepository;
 import vn.hoidanit.jobhunter.repository.SubscriberRepository;
 
@@ -16,14 +19,19 @@ public class SubscriberService {
 
     private final SubscriberRepository subscriberRepository;
     private final SkillRepository skillRepository;
+    private final EmailService emailService;
+    private final JobRepository jobRepository;
 
-    public SubscriberService(SubscriberRepository subscriberRepository, SkillRepository skillRepository) {
+    public SubscriberService(SubscriberRepository subscriberRepository, SkillRepository skillRepository,
+            EmailService emailService, JobRepository jobRepository) {
         this.subscriberRepository = subscriberRepository;
         this.skillRepository = skillRepository;
+        this.emailService = emailService;
+        this.jobRepository = jobRepository;
     }
 
     public boolean isExistsByEmail(String email) {
-        return this.subscriberRepository.isExistsByEmail(email);
+        return this.subscriberRepository.existsByEmail(email);
     }
 
     public Subscriber create(Subscriber subs) {
@@ -55,5 +63,46 @@ public class SubscriberService {
         }
         return this.subscriberRepository.save(subsDB);
     }
+
+    public ResEmailJob convertJobToSendEmail(Job job) {
+        ResEmailJob res = new ResEmailJob();
+        res.setName(job.getName());
+        res.setSalary(job.getSalary());
+        res.setCompany(new ResEmailJob.CompanyEmail(job.getCompany().getName()));
+        List<Skill> skills = job.getSkills();
+        List<ResEmailJob.SkillEmail> s = skills.stream().map(skill -> new ResEmailJob.SkillEmail(skill.getName()))
+                .collect(Collectors.toList());
+        res.setSkills(s);
+        return res;
+    }
+
+    public void sendSubscribersEmailJobs() {
+        List<Subscriber> listSubs = this.subscriberRepository.findAll();
+        if (listSubs != null && listSubs.size() > 0) {
+            for (Subscriber sub : listSubs) {
+                List<Skill> listSkills = sub.getSkills();
+                if (listSkills != null && listSkills.size() > 0) {
+                    List<Job> listJobs = this.jobRepository.findBySkillsIn(listSkills);
+                    if (listJobs != null && listJobs.size() > 0) {
+
+                        List<ResEmailJob> arr = listJobs.stream().map(
+                                job -> this.convertJobToSendEmail(job)).collect(Collectors.toList());
+
+                        this.emailService.sendEmailFromTemplateSync(
+                                sub.getEmail(),
+                                "Cơ hội việc làm hot đang chờ đón bạn, khám phá ngay",
+                                "job",
+                                sub.getName(),
+                                arr);
+                    }
+                }
+            }
+        }
+    }
+
+    // @Scheduled(cron = "*/10 * * * * *")
+    // public void testCron() {
+    // System.out.println(">>> TEST CRON");
+    // }
 
 }
